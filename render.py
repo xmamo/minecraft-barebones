38fe2492682f2346520a27538da4372c471e6fa0
Matteo Morena <8678043+xmamo@users.noreply.github.com>
2017-09-27 18:48:57 +0200

    Improve the way rendering works

--- render.py
+++ render.py
@@ -1,10 +1,13 @@
+import os
 import subprocess
 import sys
 from pathlib import Path
 
-for script in Path(__file__).parent.glob('**/*.py'):
-	if script != Path(__file__):
-		print('Rendering {}...'.format(str(script.with_suffix('.mcfunction'))), end = '')
-		with script.with_suffix('.mcfunction').open('w') as mcfunction:
-			subprocess.run([sys.executable, str(script)], stdout = mcfunction)
+file = Path(os.path.realpath(__file__))
+for script in file.parent.glob('**/*.py'):
+	if script != file and script.suffixes == ['.2render', '.py']:
+		mcfunction = script.with_suffix('').with_suffix('.mcfunction')
+		print('Rendering {}...'.format(str(mcfunction)), end = '')
+		with mcfunction.open('w') as f:
+			subprocess.run([sys.executable, str(script)], stdout = f)
 		print(' DONE')
