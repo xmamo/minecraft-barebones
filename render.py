4ba73409654cb38c3df41b92751ca1e259868831
Matteo Morena <8678043+xmamo@users.noreply.github.com>
2017-09-27 18:57:01 +0200

    Change ".2render.py" suffix to ".mcfunction.py"

--- render.py
+++ render.py
@@ -5,8 +5,8 @@ from pathlib import Path
 
 file = Path(os.path.realpath(__file__))
 for script in file.parent.glob('**/*.py'):
-	if script != file and script.suffixes == ['.2render', '.py']:
-		mcfunction = script.with_suffix('').with_suffix('.mcfunction')
+	if script != file and script.suffixes == ['.mcfunction', '.py']:
+		mcfunction = script.with_suffix('')
 		print('Rendering {}...'.format(str(mcfunction)), end = '')
 		with mcfunction.open('w') as f:
 			subprocess.run([sys.executable, str(script)], stdout = f)
